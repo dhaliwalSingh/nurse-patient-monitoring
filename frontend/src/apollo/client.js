872b5557b6e5a377044b3296e63e8055a3412c5b
dhaliwalsingh <hsin1520@my.centennialcollege.ca>
2025-04-18 21:50:09 -0600

    backend deploy setting

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT || "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
